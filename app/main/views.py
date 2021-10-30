49a6f51e9def47856e4474fe38de595860b20746
sling254 <mwanikidenis7@gmail.com>
2021-10-30 12:34:18 +0300

    add more sources to display to the users

--- app/main/views.py
+++ app/main/views.py
@@ -11,8 +11,11 @@ def index():
     a function to view  the home page 
     """
     technology = get_news_source('technology')
+    sports = get_news_source('sports')
+    business = get_news_source('business')
+    science = get_news_source('science')
     title = "News Home Source"
-    return render_template('index.html', title=title, technology=technology)
+    return render_template('index.html', title=title, technology=technology, sports=sports,business=business, science=science)
 
 
 
